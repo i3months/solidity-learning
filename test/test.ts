c0c7bde577f24af0c495019d12b9345af948bd34
jmj <waysoleward01@naver.com>
2025-03-25 21:52:26 +0900

    feat: ethers를 사용한 트랜잭션 테스트

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,7 @@
 // hardhat 프레임워크 안에서 실행해 보기 위해
 
 import hre from "hardhat";
+import { ethers, Wallet } from "ethers";
 
 describe("hardhat-test", () => {
     it("print hardhat", async () => {
@@ -53,6 +54,36 @@ describe("hardhat-test", () => {
     });
 
     it("ethers test", async () => {
+        // bob의 서명을 받아야 보낼 수 있도록.. 
+        // ethers에서는 내부적으로 서명을 해 줌 (편의상)
+        // nonce 가 0이면 bob이 만든 0 번째 트랜잭션을 의미 
 
+        // npx hardhat node 로 만들어진 key를 가져와서 사용 
+        const provider = new ethers.JsonRpcProvider("http://localhost:8545");
+
+        const bobWallet = new ethers.Wallet(
+            "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+            provider
+        );
+        const aliceWallet = new ethers.Wallet(
+            "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
+        );
+
+        const tx = {
+            from: bobWallet.address,
+            to: aliceWallet.address,
+            value: ethers.parseEther("100"),
+            // chainId: 31337,
+        }
+
+        // ethers를 사용하는 경우 자동으로 채워주지 않으니.. populatedTx 사용해서 채워주기 
+        // 서명은 bobWallet으로, 이후 블록체인 노드로 보내주자.
+        const populatedTx = await bobWallet.populateTransaction(tx);
+        const signedTx = await bobWallet.signTransaction(populatedTx);
+        const txHash = await provider.send("eth_sendRawTransaction", [signedTx]);
+
+        // 수수료는 송신자가 지불하게 됨 
+        console.log(await provider.getBalance(bobWallet.address));
+        console.log(await provider.getBalance(aliceWallet.address));
     });
 });
