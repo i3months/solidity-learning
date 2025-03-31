1651c73a327466a2d929e3712b9ae58b14e5e61a
jmj <waysoleward01@naver.com>
2025-03-31 14:33:07 +0900

    feat: 관련 함수 테스트

--- test/MyToken.ts
+++ test/MyToken.ts
@@ -1,12 +1,15 @@
 import hre from "hardhat";
 import { expect } from "chai";
 import { MyToken } from "../typechain-types";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
 // 컴파일 할 때 필요한 클래스 만들어줌 
 describe("mytoken deploy", () => {
     let myTokenContract: MyToken;
+    let signers: HardhatEthersSigner[];
 
     before("should deploy", async () => {
+        signers = await hre.ethers.getSigners();
         myTokenContract = await hre.ethers.deployContract("MyToken", [
             "MyToken",
             "MT",
@@ -23,4 +26,11 @@ describe("mytoken deploy", () => {
     it("should return decimals", async () => {
         expect(await myTokenContract.decimals()).equal(18);
     })
+    it("should return 0 totalSupply", async () => {
+        expect(await myTokenContract.totalSupply()).equals(0);
+    })
+    it("should return 0 balance for signer 0", async () => {
+        expect(await myTokenContract.balanceOf(signers[0])).equals(0);
+    })
+
 })
