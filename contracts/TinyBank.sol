a680578c317f868150f61588eebad14ec8e6e458
jmj <waysoleward01@naver.com>
2025-04-30 14:42:32 +0900

    feat: TinyBank 구현

---
+++ contracts/TinyBank.sol
@@ -0,0 +1,35 @@
+// staking 
+// deposit(MyToken) / withdraw(MyToken)
+
+// SPDX-License-Identifier: MIT
+
+// vault (금고)
+pragma solidity ^0.8.28;
+
+interface IMyToken {
+    function transfer(uint256 amount, address to) external;
+
+    function transferFrom(address from, address to, uint256 amount) external;
+}
+
+// 저장할 token을 먼저 배포, 이후 TinyBank의 생성자로 줘야 함 
+contract TinyBank {
+    event Staked(address, uint256);
+
+    IMyToken public stakingToken;
+    mapping(address => uint256) staked;
+    uint256 totalstaked;
+
+    constructor(IMyToken _stakingToken) {
+        stakingToken = _stakingToken;
+    }
+
+    function stake(uint256 _amount) external {
+        require(_amount >= 0, "cannot stake 0 amount");
+        stakingToken.transferFrom(msg.sender, address(this), _amount);
+        staked[msg.sender] += _amount;
+        totalStaked += _amount;
+        emit Staked(msg.sender, _amount);
+    }
+}
+
