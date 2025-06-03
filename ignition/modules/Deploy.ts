02ffa1939e883b1276db0c51766d934dd75dc051
jmj <waysoleward01@naver.com>
2025-06-03 12:42:39 +0900

    feat: 트랜잭션 배포 세팅

---
+++ ignition/modules/Deploy.ts
@@ -0,0 +1,23 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+// 배포 테스트 하려면 hardhat 네트워크 띄워 둬야 함
+// npx hardhat ignition deploy ignition/modules/MyToken.ts --network localhost
+export default buildModule("MyTokenDeploy", (m) => {
+  const initialAmount = BigInt(1_000_000);
+
+  const myTokenContract = m.contract("MyToken", [
+    "MyToken",
+    "MT",
+    18,
+    initialAmount,
+  ]);
+
+  const managerAccount = m.getAccount(0);
+  const managerList = [managerAccount];
+
+  const tinyBanckContract = m.contract("TinyBank", [
+    myTokenContract,
+    managerList,
+  ]);
+  return { myTokenContract, tinyBanckContract };
+});
